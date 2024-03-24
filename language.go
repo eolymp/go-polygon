185ced30af8e7c70eafca46567c85b6ac51d4076
Anton Tsypko <tsypko@berkeley.edu>
2024-03-24 14:31:21 +0100

    Update language.go

--- language.go
+++ language.go
@@ -4,10 +4,16 @@ import "fmt"
 
 func LocaleFromLanguage(lang string) (string, error) {
 	switch lang {
-	case "ukrainian", "russian", "english", "hungarian":
+	case "ukrainian", "russian", "english", "hungarian", "azerbaijani", "french":
 		return lang[:2], nil
 	case "kazakh":
 		return "kk", nil
+	case "spanish":
+		return "es", nil
+	case "polish":
+		return "pl", nil
+	case "german":
+		return "de", nil
 	default:
 		return lang, fmt.Errorf("unknown language %#v", lang)
 	}
