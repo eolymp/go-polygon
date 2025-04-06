671a35fc8dae1362ceb334b2f545ac358dfb7244
Anton Tsypko <antontsypko@gmail.com>
2025-04-06 21:40:31 +0100

    Update language.go

--- language.go
+++ language.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func LocaleFromLanguage(lang string) (string, error) {
 	switch lang {
-	case "ukrainian", "russian", "english", "hungarian", "azerbaijani", "french", "arabic", "uzbek":
+	case "ukrainian", "russian", "english", "hungarian", "azerbaijani", "french", "arabic", "uzbek", "slovene":
 		return lang[:2], nil
+	case "armenian":
+		return "hy", nil
+	case "lithuanian":
+		return "lt", nil
+	case "serbian":
+		return "sr", nil
 	case "kazakh":
 		return "kk", nil
 	case "spanish":
